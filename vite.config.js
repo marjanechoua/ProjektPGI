38b97ed0465190295f662c8732cb4a558cf5eecb
Marjane Choua <147026189+marjanechoua@users.noreply.github.com>
2024-02-07 11:34:20 +0100

    add files to repository

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://retoolapi.dev/gaNPMO/istudymap',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace('/api', '')
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
